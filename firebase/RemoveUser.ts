5b1c97c331d3cefee12be17d05352a146dc3b998
YuvalYarmus <yarmusyuval@gmail.com>
2021-06-12 14:53:09 +0300

    made removeUser properly use Promise

--- firebase/RemoveUser.ts
+++ firebase/RemoveUser.ts
@@ -2,17 +2,21 @@ import firebase from "firebase/app";
 import { User } from "./types"
 
 export default async function removeUser(user: User) {
-  try {
-    console.log(`trying to remove a user from users`);
-    setTimeout(async () => {
-      await firebase.firestore().collection("users").doc(`${user.id}`).delete();
-    });
-    return new Promise<void>((resolve) => {
-      console.log(`removed a user from users (from resolve)`);
-      resolve();
-    });
-  } catch (err) {
-    console.log(`got an error trying to remove a user from users :${err}`);
-    return new Promise<void>((reject) => reject());
-  }
+
+  return new Promise<boolean>(async (resolve, reject) => {
+    try {
+      console.log(`trying to remove a user from users`);
+      await firebase.firestore().collection("users").doc(`${user.id}`).delete().then(() => {
+        console.log(`managed to delete the user from users`);
+        resolve(true);
+      }).catch((e) => {
+        console.log(`failed to remove user from users`);
+        resolve(false);
+      });
+    } catch (e) {
+      console.log(`got an error trying to remove a user from users :${e}`);
+      return new Promise<void>((reject) => reject());
+    }
+  });
+
 }
