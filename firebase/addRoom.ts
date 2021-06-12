266eb966f5014cf8c9aaeef79ed4b4deb7e0f4cf
YuvalYarmus <yarmusyuval@gmail.com>
2021-06-12 19:54:27 +0300

    removed currTurn from room Schema

--- firebase/addRoom.ts
+++ firebase/addRoom.ts
@@ -24,7 +24,7 @@ export default async function addRoomToFirestore(room: Room) {
             users: [],
             n: room.n,
             m: room.m,
-            currTurn: 0,
+            // currTurn: 0,
           },
           { merge: true }
         );
