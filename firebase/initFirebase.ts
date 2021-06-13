0b6073d7862392769980bd471a68252084e80764
YuvalYarmus <yarmusyuval@gmail.com>
2021-06-13 18:31:04 +0300

    should check if it is possible to start the firebase connection on server side, if not than we will use the next public to get it on the client side as well

--- firebase/initFirebase.ts
+++ firebase/initFirebase.ts
@@ -1,42 +1,40 @@
-import firebase from 'firebase/app'
+import firebase from "firebase/app";
 // the below imports are option - comment out what you don't need
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCt39LFoPbOpK8-IbrqutUhKEP1bH7MC9k",
-    authDomain: "chomp-9a0f3.firebaseapp.com",
-    projectId: "chomp-9a0f3",
-    storageBucket: "chomp-9a0f3.appspot.com",
-    messagingSenderId: "785308785684",
-    appId: "1:785308785684:web:b8f96fd8de1619db865b01"
+  apiKey: "AIzaSyCt39LFoPbOpK8-IbrqutUhKEP1bH7MC9k",
+  authDomain: "chomp-9a0f3.firebaseapp.com",
+  projectId: "chomp-9a0f3",
+  storageBucket: "chomp-9a0f3.appspot.com",
+  messagingSenderId: "785308785684",
+  appId: "1:785308785684:web:b8f96fd8de1619db865b01",
+};
+
+let envConfig: Object | null = null;
+if (process.env.apiKey) {
+  envConfig = {
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
   };
-
-  let envConfig: { apiKey: string; authDomain: string | undefined; projectId: string | undefined; storageBucket: string | undefined; messagingSenderId: string | undefined; appId: string | undefined; } | null = null;
-  if (process.env.apiKey) {
-      envConfig = {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId
-      };
-  }
-
-export default function initFirebase() {
-    console.log(`in firebase init function`)
-    if (!firebase.apps.length) {
-        if (envConfig != null) {
-            console.log(`MAKING AN INIT WITH ENVIRONMENT VARIABLES`)
-            firebase.initializeApp(envConfig);
-        }
-        else firebase.initializeApp(firebaseConfig);
-        console.log(`Firebase was successfully init. envConfig: ${envConfig}`);
-    }
 }
 
+export default function initFirebase() {
+  console.log(`in firebase init function`);
+  if (!firebase.apps.length) {
+    if (envConfig != null) {
+      console.log(`MAKING AN INIT WITH ENVIRONMENT VARIABLES`);
+      firebase.initializeApp(envConfig);
+    } else firebase.initializeApp(firebaseConfig);
+    console.log(`Firebase was successfully init. envConfig: ${envConfig}`);
+  }
+}
 
 // authenticate the first user on the loading page
 //
